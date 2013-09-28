6b627696985dd6ed159464b1a9289267eacf5553
Joel Micha Krebs <joel.krebs@students.unibe.ch>
2013-09-28 16:19:01 +0200

    initial commit

---
+++ external-author.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: External Author
+Plugin URI: http://www.aleaiactaest.ch
+Description: Allows publishing of posts with an author possible who does not have a wordpress account.
+Version: 0.1
+Author: Joel Krebs
+Author URI: http://www.aleaiactaest.ch
+License: GPL2
+*/
