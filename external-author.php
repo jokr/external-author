073a8271b82904036de7db59d5648b51bc1dbe77
Steffen Baumgart <steffen@baumgart.biz>
2015-07-21 21:17:30 +0200

    added "featured author" selection to plugin panel

--- external-author.php
+++ external-author.php
@@ -3,8 +3,8 @@
 Plugin Name: External Author
 Plugin URI: http://www.aleaiactaest.ch
 Description: Allows publishing of posts with an author possible who does not have a wordpress account.
-Version: 0.2
-Author: Joel Krebs
+Version: 0.3
+Author: Joel Krebs, Steffen Baumgart
 Author URI: http://www.aleaiactaest.ch
 License: GPL2
 */
